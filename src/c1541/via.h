82d2983ff83dcbb7898865f8287755715ebfd0e9
Thomas Giesel <skoe@directbox.com>
2008-09-03 22:08:16 +0200

    Files for VIA added
    But they are still empty.
    
    committer: Thomas Giesel <skoe@directbox.com>

---
+++ src/c1541/via.h
@@ -0,0 +1,40 @@
+/*
+ * (c) 2008 Thomas Giesel <skoe@directbox.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License only.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/* MOS 6522 registers */
+#define VIA_PRB         0  /* Port B */
+#define VIA_PRA         1  /* Port A */
+#define VIA_DDRB        2  /* Data direction register for port B */
+#define VIA_DDRA        3  /* Data direction register for port A */
+
+#define VIA_T1CL        4  /* Timer 1 count low */
+#define VIA_T1CH        5  /* Timer 1 count high */
+#define VIA_T1LL        6  /* Timer 1 latch low */
+#define VIA_T1LH        7  /* Timer 1 latch high */
+#define VIA_T2CL        8  /* Timer 2 count low - read only */
+#define VIA_T2LL        8  /* Timer 2 latch low - write only */
+#define VIA_T2CH        9  /* Timer 2 latch/count high */
+
+#define VIA_SR          10 /* Serial port shift register */
+#define VIA_ACR         11 /* Auxiliary control register */
+#define VIA_PCR         12 /* Peripheral control register */
+
+#define VIA_IFR         13 /* Interrupt flag register */
+#define VIA_IER         14 /* Interrupt control register */
+#define VIA_PRA_NHS     15 /* Port A with no handshake */
+
+#define VIA_REG_SIZE    16 /* Number of VIA registers */
